a5ca808179c40149f52bd0189b149eb0a37f64e1
Anshu Rai <raianshu8991@gmail.com>
2020-11-20 20:37:34 +0530

    added edit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Task} from './components/task';
+import Task from './components/task';
 class App extends Component {
 
   constructor(props){
--- src/components/task.js
+++ src/components/task.js
@@ -1,12 +1,38 @@
 // @flow 
-import * as React from 'react';
+import React, { Component } from 'react';
 
-export const Task = (props) => {
-    const {task,handleDelete}=props;
-    return (
-        <div>
+class Task extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            editMode:false,
+            updateValue:''
+        }
+    }
+    handleClick=(e)=>{
+        this.setState({editMode:true})
+    }
+    handleOnChange=(e)=>{
+        this.setState({updateValue:[e.target.value]});
+    }
+    handleSave=(e)=>{
+        const {updateValue}=this.state;
+        console.log('updated value',updateValue
+        )
+    }
+    render() {
+        const {task,handleDelete}=this.props;
+        const {editMode}=this.state;
+
+        return (
+            <div>
+         
+        {editMode?<div><input /> <button onClick={this.handleSave}>Save</button></div>:<div><p style={{display:"inline-block"}}>{task}</p> <button style={{display:"inline-block"}} onClick={()=>handleDelete(task)}>Delete</button> <button onClick={this.handleClick}>Edit </button></div>}
+    
+    </div>
         
-    <p style={{display:"inline-block"}}>{task}</p> <button style={{display:"inline-block"}} onClick={()=>handleDelete(task)}>Delete</button>
-        </div>
-    );
-};
+        );
+    }
+}
+
+export default Task;
